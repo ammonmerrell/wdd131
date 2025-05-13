8bb9319e41369639600eae86af52dd0c9dde1668
ammonmerrell <ammonmerrell@outlook.com>
2025-05-13 10:06:57 -0600

    changed "#year" to "#currentYear"

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,8 @@
-const year = document.querySelector("#year")
+const year = document.querySelector("#currentyear");
+
 
 const today = new Date();
+
 
 year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
 let text = document.lastModified;
