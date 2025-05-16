51174283e7571d95b2f441e572961c89eccd15b4
ammonmerrell <ammonmerrell@outlook.com>
2025-05-16 11:36:53 -0600

    added hamburger funtionality in .css and .js

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -4,3 +4,12 @@ year.innerHTML = ` <span class="highlight">${today.getFullYear()} </span>`;
 let modified = document.querySelector("#lastmodified");
 let text = document.lastModified;
 modified.innerHTML = ` last Modified: <span class="highlight">${text}</span>`;
+
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
