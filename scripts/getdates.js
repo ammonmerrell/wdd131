8fdd082a84bea829d0ccb07591a03394003f267d
ammonmerrell <ammonmerrell@outlook.com>
2025-05-16 10:06:16 -0600

    removed "getCurrentYear()" text from getdates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,5 +4,5 @@ const year = document.querySelector("#currentyear");
 const today = new Date();
 
 
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+year.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
 let text = document.lastModified;
