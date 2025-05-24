102d14069347a91fcdb4db6e9348425ddb0dd0d5
ammonmerrell <ammonmerrell.outlook.com>
2025-05-23 21:43:24 -0600

    Created funtional if statment.

--- scripts/place.js
+++ scripts/place.js
@@ -15,12 +15,30 @@ modified.innerHTML = ` last Modified: <span class="highlight">${text}</span>`;
 let speed = 7;
 let temp = 45;
 
+const no = "N/A";
+
 let wc = {
     calculateWindChill(speed, temp) {
         return (35.74 + 0.6215 * temp) - 35.75 * (speed ** 0.16) + 0.4275 * temp *(speed ** 0.16)
     }
 }
+if (temp <= 50) {
+    if (speed > 3) {
+        let wch = document.querySelector("#windchill");
+        wch.innerHTML = ` <span class="highlight">${wc.calculateWindChill(speed, temp)} </span>`;
+
+    }
+    else {
+        let wch = document.querySelector("#windchill");
+        wch.innerHTML = ` <span class="highlight">${no} </span>`;
+        
+    }
+}
+else {
+    let wch = document.querySelector("#windchill");
+    wch.innerHTML = ` <span class="highlight">${no} </span>`;
+    
+}
 
 
-let wch = document.querySelector("#windchill"); 
-wch.innerHTML = ` <span class="highlight">${wc.calculateWindChill(speed, temp)} </span>`;
+
