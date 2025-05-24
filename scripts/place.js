8e48f2fced3af92d66b241657b0c20b31e412a6e
ammonmerrell <ammonmerrell.outlook.com>
2025-05-23 21:20:49 -0600

    added windchill fuctionality and hero img via absolute positiong

--- scripts/place.js
+++ scripts/place.js
@@ -9,3 +9,18 @@ year.innerHTML = ` <span class="highlight">${today.getFullYear()} </span>`;
 let modified = document.querySelector("#lastmodified");
 let text = document.lastModified;
 modified.innerHTML = ` last Modified: <span class="highlight">${text}</span>`;
+
+
+
+let speed = 7;
+let temp = 45;
+
+let wc = {
+    calculateWindChill(speed, temp) {
+        return (35.74 + 0.6215 * temp) - 35.75 * (speed ** 0.16) + 0.4275 * temp *(speed ** 0.16)
+    }
+}
+
+
+let wch = document.querySelector("#windchill"); 
+wch.innerHTML = ` <span class="highlight">${wc.calculateWindChill(speed, temp)} </span>`;
