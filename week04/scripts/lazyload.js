28bf992b7adc9b13ca12b504e69636031c9ef901
ammonmerrell <ammonmerrell.outlook.com>
2025-05-27 12:00:06 -0600

    added three files for lazyload

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,11 @@
+const year = document.querySelector("#currentyear");
+
+
+const today = new Date();
+
+
+year.innerHTML = ` <span class="highlight">${today.getFullYear()} </span>`;
+
+let modified = document.querySelector("#lastmodified");
+let text = document.lastModified;
+modified.innerHTML = ` last Modified: <span class="highlight">${text}</span>`;
