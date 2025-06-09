82feff48bd6b0f76cce9dc46460aec948797efe5
ammonmerrell <ammonmerrell.outlook.com>
2025-06-09 15:45:05 -0600

    fixed js

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,6 +1,6 @@
 const year = document.querySelector('#currentyear');
 const today = new Date();
-year.innerHTML = `<span class="Highlight">${today.getFullYear} </span>`;
+year.innerHTML = `<span class="Highlight">${today.getFullYear()} </span>`;
 let modified = document.querySelector('#lastmodified');
 let text = document.lastModified;
-modified.innerHTML = `<span class="highlight">${text}</span>`;
+modified.innerHTML = `last modified: <span class="highlight">${text}</span>`;
