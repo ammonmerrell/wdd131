4cb0e37fe823a9ab34d02f311d5ddca1ae3126f5
ammonmerrell <ammonmerrell.outlook.com>
2025-06-10 09:39:13 -0600

    added site visiting to local storage on index.html

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -4,3 +4,15 @@ year.innerHTML = `<span class="Highlight">${today.getFullYear()} </span>`;
 let modified = document.querySelector('#lastmodified');
 let text = document.lastModified;
 modified.innerHTML = `last modified: <span class="highlight">${text}</span>`;
+
+const visitDisplay = document.querySelector(".visited");
+let visits = Number(window.localStorage.getItem("visits-ls"));
+if (visits == 0) {
+    visitDisplay.textContent = ` Welcome, this is your first visit!`;
+    visits++;
+} else {
+    visitDisplay.textContent = ` This has been visited ${visits} times.`;
+    visits++;
+}
+
+localStorage.setItem("visits-ls", visits);
