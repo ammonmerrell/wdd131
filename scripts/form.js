7a50e375bf1f77e39992bec3ba35d4851f7d721d
ammonmerrell <ammonmerrell.outlook.com>
2025-06-12 09:28:47 -0600

    futher fixed form .js

--- scripts/form.js
+++ scripts/form.js
@@ -34,18 +34,36 @@ const products = [
     }
 ];
 
-// findProducts(products);
-// productName = document.querySelector(".pro");
-// productName = findProducts();
-// function findProducts(item) {
-//     item.forEach(product => {
-//         let select = document.createElement("select");
-//         let name = document.createElement("option");
-//         let id = document.createElement("option");
-//         id.innerHTML = `a ${product.id}`;
-//         name.textContent = product.name;
-//         select.appendChild(name);
-//         select.appendChild(id);
-//         document.querySelector(".pro").appendChild(select);
-//     });
-// }
+findProducts(products);
+productName = document.querySelector(".pro");
+productName = findProducts();
+function findProducts(item) {
+    let select = document.createElement("select");
+    let name1 = document.createElement("option");
+    let id = document.createElement("option");
+    let name3 = document.createElement("option");
+    let name4 = document.createElement("option");
+    let name5 = document.createElement("option");
+    name1.innerHTML = `a `;
+    id.innerHTML = ` ${products[1].name}`;
+    name1.innerHTML = `${products[0].name}`;
+    name3.innerHTML = `${products[2].name}`;
+    name4.innerHTML = `${products[3].name}`;
+    name5.innerHTML = `${products[4].name}`;
+    select.appendChild(name1);
+    select.appendChild(id);
+    select.appendChild(name3);
+    select.appendChild(name4);
+    select.appendChild(name5);
+    document.querySelector(".pro").appendChild(select);
+    // item.forEach(product => {
+    //     let select = document.createElement("select");
+    //     let name = document.createElement("option");
+    //     let id = document.createElement("option");
+    //     id.innerHTML = `a ${product.id}`;
+    //     name.textContent = product.name;
+    //     select.appendChild(name);
+    //     select.appendChild(id);
+    //     document.querySelector(".pro").appendChild(select);
+    // });
+}
