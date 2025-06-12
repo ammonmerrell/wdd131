3f927c4f9205682c29c6ead066e573915b08d791
ammonmerrell <ammonmerrell.outlook.com>
2025-06-12 08:04:39 -0600

    added template literal for starting message

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -8,7 +8,7 @@ modified.innerHTML = `last modified: <span class="highlight">${text}</span>`;
 const visitDisplay = document.querySelector(".visited");
 let visits = Number(window.localStorage.getItem("visits-ls"));
 if (visits == 0) {
-    visitDisplay.textContent = ` Welcome, this is your first visit!`;
+    visitDisplay.textContent = ` Welcome, you have seen this page ${visits} times!`;
     visits++;
 } else {
     visitDisplay.textContent = ` This has been visited ${visits} times.`;
